2951aa362ff4296192668913f57ffa49b8341319
Anna_Zagrebina <eto-projdet@yandex.ru>
2023-11-17 18:45:23 +0300

    Add class BookServiceTest

--- Test/seminars/fourth/book/BookServiceTest.java
+++ Test/seminars/fourth/book/BookServiceTest.java
@@ -1,7 +1,40 @@
 package seminars.fourth.book;
 
+import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class BookServiceTest {
+    @Test
+    public void testFindBookById() {
 
+        BookRepository mockRepository = mock(BookRepository.class);
+        when(mockRepository.findById("123")).thenReturn(new Book("123", "Test Book", "Test Author"));
+        BookService bookService = new BookService(mockRepository);
+
+        Book result = bookService.findBookById("123");
+
+        assertEquals("Test Book", result.getTitle());
+        assertEquals("Test Author", result.getAuthor());
+    }
+
+    @Test
+    public void testFindAllBooks() {
+
+        BookRepository mockRepository = mock(BookRepository.class);
+        when(mockRepository.findAll()).thenReturn(List.of(new Book("123", "Test Book", "Test Author")));
+
+        BookService bookService = new BookService(mockRepository);
+
+        List<Book> result = bookService.findAllBooks();
+
+        assertEquals(1, result.size());
+        assertEquals("Test Book", result.get(0).getTitle());
+        assertEquals("Test Author", result.get(0).getAuthor());
+    }
 }
